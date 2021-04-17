66670c8cb3eee016c69283d156daeacf15ebeab8
Dhawal Kumar Singh <dhawal.singh10@gmail.com>
2021-04-18 00:53:06 +0530

    Initial Commit

---
+++ src/components/NotFound.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const NotFound = () => {
+  return (
+    <div>
+      <p>The Page You are Looking is Not Found !</p>
+    </div>
+  );
+};
+
+export default NotFound;
---
+++ src/components/NotesList.js
@@ -0,0 +1,38 @@
+import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import NotesService from "../services/NotesService";
+const NotesList = () => {
+  const [notes, setNotes] = useState([]);
+
+  useEffect(() => {
+    NotesService.getAll()
+      .then((response) => {
+        setNotes(response.data);
+      })
+      .catch((error) => {
+        console.log("Something Went Wrong", error);
+      });
+  }, []);
+  return (
+    <div className="main-content">
+      <h4>List Of Notes </h4>
+      <div className="notes-list mt-4">
+        {" "}
+        {notes.length > 0 ? (
+          notes.map((note) => (
+            <div key={note.id} className="notes-preview mt-3 ">
+              <Link to="#">
+                <h5 className="primary-color text-capitalize">{note.title}</h5>
+                <p>{note.body}</p>
+              </Link>
+            </div>
+          ))
+        ) : (
+          <div>No notes available.</div>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default NotesList;
